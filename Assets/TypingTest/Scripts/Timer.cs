68ff4f07d7d9bfc2e058c7bac60ef074d4a254d3
Cooper Saye <coopersaye@gmail.com>
2020-08-31 18:45:16 -0700

    Implement countdown timer.

--- Assets/TypingTest/Scripts/Timer.cs
+++ Assets/TypingTest/Scripts/Timer.cs
@@ -1,9 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 namespace TypingTest
 {
+    [RequireComponent(typeof(TextMeshProUGUI))]
     public class Timer : MonoBehaviour
     {
-        
+        private TextMeshProUGUI textField;
+
+        private Coroutine countdown;
+
+        private int seconds;
+
+        private void Start()
+        {
+            textField = GetComponent<TextMeshProUGUI>();
+
+            StartCountdown();
+        }
+
+        private void StartCountdown()
+        {
+            if (countdown != null)
+            {
+                StopCoroutine(countdown);
+            }
+
+            countdown = StartCoroutine(Countdown());
+        }
+
+        private void StopCountdown()
+        {
+            if (countdown != null)
+            {
+                StopCoroutine(countdown);
+            }
+        }
+
+        private IEnumerator Countdown()
+        {
+            for (int i = 60; i > 0; i--)
+            {
+                seconds = i;
+                UpdateTextField();
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        private void UpdateTextField()
+        {
+            textField.text = seconds.ToString();
+        }
     }
 }
