1d03b77b0168294bf8fd36b5f16cad59fa85885d
Cooper Saye <coopersaye@gmail.com>
2020-08-31 22:47:00 -0700

    Implement error text.

---
+++ Assets/TypingTest/Scripts/ErrorText.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace TypingTest
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class ErrorText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private TextInput textInput = null;
+        [SerializeField] private TextToWrite textToWrite = null;
+
+        public int errors {get; private set;} = 0;
+
+        private TextMeshProUGUI textField;
+
+        private void Start()
+        {
+            textField = GetComponent<TextMeshProUGUI>();
+        }
+
+        public void UpdateText()
+        {
+            StringBuilder sb = new StringBuilder();
+            errors = 0;
+
+            for (int i = 0; i < textInput.text.Length; i++)
+            {
+                if (textInput.text[i] == textToWrite.text[i])
+                {
+                    sb.Append(textInput.text[i]);
+                }
+                else
+                {
+                    sb.Append($"<#FF0000>{textInput.text[i]}</color>");
+                    errors++;
+                }
+            }
+
+            textField.text = sb.ToString();
+        }
+    }
+}
--- Assets/TypingTest/Scripts/TextInput.cs
+++ Assets/TypingTest/Scripts/TextInput.cs
@@ -7,128 +7,29 @@ namespace TypingTest
     public class TextInput : MonoBehaviour
     {
         [Header("References")]
-        [SerializeField] private TextToWrite textToWrite = null;
+        [SerializeField] private ErrorText errorText = null;
 
-        private const string incorrectCharStart = "<#FF0000>";
-        private const string incorrectCharEnd = "</color>";
+        public string text
+        {
+            get
+            {
+                return inputField.text;
+            }
+        }
+
+        public int validChars
+        {
+            get
+            {
+                return text.Length - errorText.errors;
+            }
+        }
 
         private TMP_InputField inputField;
 
         private void Start()
         {
             inputField = GetComponent<TMP_InputField>();
-        }
-
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift)) UpdateText();
-        }
-
-        public void UpdateText()
-        {
-            string inputText = InputText();
-
-            for (int i = 0; i < inputText.Length; i++)
-            {
-                if (inputText[i] != textToWrite.text[i] && !IsMarkedIncorrect(i))
-                {
-                    MarkIncorrect(i);
-                }
-            }
-        }
-
-        private bool IsMarkedIncorrect(int index)
-        {
-            int rawIndex = InputTextToRaw(index);
-            string rawInputText = inputField.text;
-
-            try
-            {
-                if (rawInputText.Substring(rawIndex + 1, rawIndex + 1 + incorrectCharEnd.Length) == incorrectCharEnd)
-                {
-                    if (rawInputText.Substring(rawIndex - 1 - incorrectCharStart.Length, rawIndex - 1) == incorrectCharStart)
-                    {
-                        return true;
-                    }
-                }
-            }
-            catch
-            {
-                return false;
-            }
-
-            return false;
-        }
-
-        private void MarkIncorrect(int index)
-        {
-            int rawIndex = InputTextToRaw(index);
-
-            string rawInputText = inputField.text;
-
-            rawInputText = rawInputText.Insert(index + 1, incorrectCharEnd);
-            rawInputText = rawInputText.Insert(index, incorrectCharStart);
-
-            inputField.text = rawInputText;
-        }
-
-        private int InputTextToRaw(int index)
-        {
-            int rawIndex = index;
-            string rawInputText = inputField.text;
-
-            bool checkingModifiers = true;
-            
-            while (checkingModifiers)
-            {
-                if (rawInputText.Substring(0, index).Contains(incorrectCharStart))
-                {
-                    rawInputText = RemoveString(incorrectCharStart, rawInputText);
-                    rawIndex += incorrectCharStart.Length;
-                }
-                else if (rawInputText.Substring(0, index).Contains(incorrectCharEnd))
-                {
-                    rawInputText = RemoveString(incorrectCharEnd, rawInputText);
-                    rawIndex += incorrectCharEnd.Length;
-                }
-                else
-                {
-                    checkingModifiers = false;
-                }
-            }
-
-            return rawIndex;
-        }
-
-        private string RemoveString(string toRemove, string baseString)
-        {
-            if (!baseString.Contains(toRemove)) return baseString;
-
-            string newString = baseString;
-
-            int removeIndex = newString.IndexOf(toRemove);
-
-            for (int i = 0; i < toRemove.Length; i++)
-            {
-                newString.Remove(removeIndex);
-            }
-
-            return newString;
-        }
-
-        private string InputText()
-        {
-            string inputText = inputField.text;
-            
-            inputText = inputText.Replace(incorrectCharStart, "");
-            inputText = inputText.Replace(incorrectCharEnd, "");
-            
-            return inputText;
-        }
-
-        public int CharsTyped()
-        {
-            return InputText().Length;
         }
     }
 }
