46d39c7416d2f80a329a29126f8136b6018b475d
Cooper Saye <coopersaye@gmail.com>
2020-08-31 18:35:41 -0700

    Implement basic random word generation.

--- Assets/TypingTest/Scripts/TextToWrite.cs
+++ Assets/TypingTest/Scripts/TextToWrite.cs
@@ -1,9 +1,59 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace TypingTest
 {
+    [RequireComponent(typeof(TextMeshProUGUI))]
     public class TextToWrite : MonoBehaviour
     {
-        
+        [SerializeField] private string[] words = new string[1000];
+
+        private TextMeshProUGUI textField;
+
+        private const int initialTextLines = 8;
+        private const int charsToLine = 30;
+
+        private void Start()
+        {
+            textField = GetComponent<TextMeshProUGUI>();
+
+            InitializeWords();
+
+            for (int i = 0; i < initialTextLines; i++)
+            {
+                PrintLine();
+            }
+        }
+
+        private void InitializeWords()
+        {
+            TextAsset wordsFile = (TextAsset)Resources.Load("Words");
+            string wordsText = wordsFile.text;
+            words = wordsText.Split('\n');
+        }
+
+        private string GetRandomWord()
+        {
+            int randomIndex = Random.Range(0, words.Length);
+            return words[randomIndex];
+        }
+
+        private void PrintLine()
+        {
+            int charsAdded = 0;
+
+            while (charsAdded < charsToLine)
+            {
+                charsAdded += PrintWord();
+            }
+        }
+
+        // Returns length added to text field
+        private int PrintWord()
+        {
+            string newWord = $"{GetRandomWord()} ";
+            textField.text += newWord;
+            return newWord.Length;
+        }
     }
 }
